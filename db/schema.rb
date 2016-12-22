6348d89176c131f869817a146c833afd6d4509e3
Ismail <ismail@unik.ma>
2016-12-22 16:51:51 +0100

    Add models and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123221751) do
+ActiveRecord::Schema.define(version: 20161222153302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       default: "", null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title",       default: "", null: false
+    t.text     "body",        default: "", null: false
+    t.string   "url",         default: "", null: false
+    t.integer  "category_id"
+    t.integer  "user_id"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.index ["category_id"], name: "index_posts_on_category_id", using: :btree
+    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,4 +50,6 @@ ActiveRecord::Schema.define(version: 20161123221751) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "posts", "categories"
+  add_foreign_key "posts", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,34 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(email: 'ismail@unik.ma', password: "aqwzsxedc", password_confirmation: "aqwzsxedc")
+categories = Category.create([{ name: 'Healthy' }, { name: 'Wealthy' }, { name: 'Wise' }])
+posts = Post.create([
+        {
+          title: 'Upgrade Your Energy, Optimize Your Supplements',
+          body: 'Article that helps know which supplements are necessary, recommedned dosage and brands.',
+          url: 'https://blog.bulletproof.com/optimize-your-supplements',
+          category_id: 1,
+          user_id: 1
+        },
+        {
+          title: 'Becoming the Best Version of You',
+          body: 'This is a special episode of The Tim Ferriss Show. The audio is from a live conversation with not one, not two, but three guests: Josh Waitzkin, Ramit Sethi, and Adam Robinson.',
+          url: 'http://fourhourworkweek.com/2016/12/20/becoming-the-best-version-of-you/',
+          category_id: 3,
+          user_id: 1
+        },
+        {
+          title: 'The Tim Ferriss Radio Hour: Meditation, Mindset, and Mastery',
+          body: 'After 200 conversations with a variety of fascinating people, including Arnold Schwarzenegger, Jamie Foxx, Tony Robbins, Maria Popova, Peter Thiel, Marc Andreessen, Amanda Palmer, Malcolm Gladwell, Rick Rubin, Reid Hoffman, Chase Jarvis, Sam Harris, Rainn Wilson, and so many others, I started to spot patterns. This is the premise of my new book Tools of Titans, which is a compilation of all of my favorite habits, philosophies, and tools of world-class performers.',
+          url: 'http://fourhourworkweek.com/2016/11/16/the-tim-ferriss-radio-hour-meditation-mindset-and-mastery/',
+          category_id: 3,
+          user_id: 1
+        },
+        {
+          title: 'A question I ask new entrepreneurs',
+          body: 'Great advise from Jason Fried for entrepreneurs, for instance, working part time at Starbucks to better understand your customers.',
+          url: 'https://m.signalvnoise.com/a-question-i-ask-new-entrepreneurs-654c0da62a3b#.10x9t4lp6',
+          category_id: 2,
+          user_id: 1
+        }
+      ])
